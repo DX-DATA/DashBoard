a6d1a4c853b694f17d448373dfacc67c4ff313fd
markyul <pig68111000@gmail.com>
2021-12-03 14:12:31 +0900

    고소차 data Elecar.vue 파일에서 불러오기
    route에 props로 data 보냄

---
+++ src/components/elecar/UsingTable.vue
@@ -0,0 +1,42 @@
+<template>
+  <article class="card grid-item">
+    <h4>현재 사용중인 고소차</h4>
+    <table class="table table-hover">
+      <thead>
+        <th>고소차 ID</th>
+        <th>사용부서</th>
+        <th>사용 시작 시간</th>
+        <th>사용 종료 시간</th>
+      </thead>
+      <tr>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+      </tr>
+      <tr>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+      </tr>
+      <tr>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+        <td>dtd</td>
+      </tr>
+    </table>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'UsingTable',
+  props: {
+    state: Object,
+  },
+};
+</script>
+
+<style></style>
