13e603a67c07c5ba99eb56c31d9fddc2ee1d6edf
kwonjuhyeon <asshole96@naver.com>
2021-12-06 11:31:15 +0900

    commit 12.06
    .

--- src/components/elecar/ListTable.vue
+++ src/components/elecar/ListTable.vue
@@ -65,44 +65,18 @@
 </template>
 
 <script>
-// import { reactive } from '@vue/reactivity';
-// import axios from 'axios';
-// import api from '../../api/api';
-// import io from 'socket.io/client-dist/socket.io';
-// import { onMounted } from '@vue/runtime-core';
 import ElecarDetail from './detail/ElecarDetail.vue';
-// import { useStore } from 'vuex';
+import axios from 'axios';
+import { useStore } from 'vuex';
+import api from '../../api/api';
 export default {
   components: { ElecarDetail },
   props: {
     state: Object,
   },
   setup() {
-    let closeModal = () => {
-      document.getElementsByClassName('custom-modal')[0].style.display = 'none';
-      document.getElementsByClassName('modal-content')[0].style.display =
-        'none';
-    };
-
-    return { closeModal };
-  },
-
-    let state = reactive({
-      datas: [],
-
-      detail: '',
-      click: (data) => {
-        state.detail = data;
-        document.getElementsByClassName('custom-modal')[0].style.display =
-          'block';
-        document.getElementsByClassName('modal-content')[0].style.display =
-          'grid';
-      },
-    });
-
-    onUnmounted(() => {
-      socket.disconnect();
-    });
+    let store = useStore();
+    let url = store.getters.url;
 
     let closeModal = () => {
       document.getElementsByClassName('custom-modal')[0].style.display = 'none';
@@ -110,94 +84,6 @@ export default {
         'none';
     };
 
-  // setup() {
-  //   const store = useStore();
-  //   const url = store.getters.url;
-  //   //socket
-  //   let socket = io('http://api.dxdata.co.kr:3333');
-
-  //   let setData = (data) => {
-  //     for (let i = 0; i < state.datas.length; i++) {
-  //       if (state.datas[i].eqp_id === data[0]) {
-  //         state.datas[i].eqp_id = data[0];
-  //         state.datas[i].current_gps_lat = data[1];
-  //         state.datas[i].current_gps_lon = data[2];
-  //         state.datas[i].department = data[4];
-  //         state.datas[i].last_timestamp = data[7];
-  //       }
-  //     }
-  //   };
-
-  //   onMounted(async () => {
-  //     await axios
-  //       .get(url + '/elecar/current', {
-  //         headers: {
-  //           Authorization: 'Bearer ' + api.getCookie('auth'),
-  //         },
-  //       })
-  //       .then((response) => {
-  //         console.log(response.data);
-  //         response.data.forEach((element) => {
-  //           if (element.current_gps_lon != 0) {
-  //             state.datas.push(element);
-  //           }
-  //         });
-  //         state.originData = state.datas;
-  //       })
-  //       .catch((error) => {
-  //         console.log(error);
-  //       });
-
-  //     socket.on('new_elecar', function (data) {
-  //       setData(data);
-  //     });
-  //   });
-
-  //   let state = reactive({
-  //     datas: [],
-  //     mapOption: {
-  //       center: {
-  //         lat: 33.450701,
-  //         lng: 126.570667,
-  //       },
-  //       level: 6,
-  //     },
-  //     detail: '',
-  //     positions: [],
-  //     click: (data) => {
-  //       let param = data.eqp_id + '_' + data.last_timestamp.slice(0, 10);
-  //       document.getElementsByClassName('custom-modal')[0].style.display =
-  //         'block';
-  //       document.getElementsByClassName('modal-content')[0].style.display =
-  //         'grid';
-  //       state.detail = data;
-
-  //       axios
-  //         .get(url + '/elecar/locations?key=' + param)
-  //         .then((response) => {
-  //           state.positions = response.data;
-  //           state.mapOption.center.lat = data.current_gps_lat;
-  //           state.mapOption.center.lng = data.current_gps_lon;
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //         });
-  //     },
-  //   });
-
-  //   onUnmounted(() => {
-  //     socket.disconnect();
-  //   });
-
-  //   let closeModal = () => {
-  //     document.getElementsByClassName('custom-modal')[0].style.display = 'none';
-  //     document.getElementsByClassName('modal-content')[0].style.display =
-  //       'none';
-  //   };
-
-  //   return { state, closeModal };
-  // },
-  // watch: {},
     let checkDepartment = (data) => {
       if (localStorage.getItem('department') === data.department) {
         return true;
@@ -225,9 +111,8 @@ export default {
       }
     };
 
-    return { state, closeModal, checkDepartment, returnElecar };
+    return { closeModal, checkDepartment, returnElecar };
   },
-  watch: {},
 };
 </script>
 
